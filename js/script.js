5066f5bc0cd2c87231ff37fe038178e81515f50f
Jailson Bezerra <dreamcastfix@gmail.com>
2025-07-12 08:13:41 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,8 +4,8 @@ const RADIO_NAME = 'Jailson Web Rádio';
 const URL_STREAMING = 'https://stream.zeno.fm/yn65fsaurfhvv';
 
 //API URL /
-const API_URL = 'https://free.miradio.pro/icy?url='+URL_STREAMING;
-const FALLBACK_API_URL = 'https://free.radioapi.lat/met?url=' + URL_STREAMING;
+const API_URL = 'https://twj.es/free/?url='+URL_STREAMING;
+const FALLBACK_API_URL = 'https://twj.es/metadata/?url=' + URL_STREAMING;
 
 // Visit https://api.vagalume.com.br/docs/ to get your API key
 const API_KEY = "18fe07917957c289983464588aabddfb";
